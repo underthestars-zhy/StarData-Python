7db5e8981a7dc129a345092b5eadd0c7377e01ea
underthestars-zhy <zhuhaoyu0909@icloud.com>
2021-06-27 10:57:09 +0800

    Initial commit

---
+++ StarData.py
@@ -0,0 +1,57 @@
+from hashlib import md5
+import requests
+
+
+class BaseData:
+    api = ""
+    key = ""
+    salt = ""
+    url = ""
+    db_name = ""
+    table_name = ""
+    primary = None
+
+    def get_md5(self) -> str:
+        md5_obj = md5()
+        md5_obj.update(self.key.encode() + self.salt.encode())
+        return md5_obj.hexdigest()
+
+    def get_value(self, name):
+        try:
+            response = requests.get(
+                url=f"{self.url}/easy_get",
+                params={
+                    "api": self.api,
+                    "db_name": self.db_name,
+                    "table_name": self.table_name,
+                    "name": name,
+                    "value": self.primary,
+                },
+            )
+            return dict(response.json())['value']
+        except requests.exceptions.RequestException:
+            return None
+
+
+def get_all_items(model) -> list:
+    items = []
+    res = []
+    try:
+        response = requests.get(
+            url=f"{model.url}/easy_get_all",
+            params={
+                "api": model.api,
+                "db_name": model.db_name,
+                "table_name": model.table_name
+            },
+        )
+        items = dict(response.json())['value']
+    except requests.exceptions.RequestException:
+        items = []
+
+    for item in items:
+        m = model()
+        m.primary = item
+        res.append(m)
+
+    return res
